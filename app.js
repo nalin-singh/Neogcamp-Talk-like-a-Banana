fb876cea7547ffdc700a8e21ac6d942feb1d9a31
Nalin Singh <singhdesign@outlook.com>
2020-11-28 16:04:15 +0530

    Layout Set
    Design for the website is complete only the input and output styling and connection is left, main javascript is left

---
+++ app.js
@@ -0,0 +1,8 @@
+//message-showcase
+console.log("Sup fam boleto mera naam Vanilla JS")
+
+// //input prompt
+// var username=prompt("Working prompt script asking to enter a username")
+
+// //alert message
+// alert("Working alert notification for "+ username)
