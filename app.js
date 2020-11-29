547c23ac8ac29540c56815311bf464edff29b818
Nalin Singh <singhdesign@outlook.com>
2020-11-29 20:07:04 +0530

    Translation Compete
    Ready to converse in minion language!

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var outputText=document.querySelector("#outputText");
 
 convertButton.addEventListener('click', clickHandler)
 
+function errorMessage(error){
+    console.log(error)
+    alert("Something ain't working right!")
+}
+
 function clickHandler()
 {
-    outputText.innerText=textToConvert.value;
+    var loadUrl="https://api.funtranslations.com/translate/minion.json"+"?"+"text="+textToConvert.value;
+    fetch(loadUrl)
+    .then(response => response.json())
+    .then( json => {
+        var translatedText=json.contents.translated;
+        outputText.innerText=translatedText;
+    })
+    .catch(errorMessage)
 };
-
